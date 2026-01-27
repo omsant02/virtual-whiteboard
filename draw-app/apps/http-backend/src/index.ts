670abafa0d7362c52b6c6ebadf4b970dd3a80c29
om santoshwar <omsantoshwar02@gmail.com>
2026-01-27 21:22:06 +0530

    create http endpoints and guard ws server

--- draw-app/apps/http-backend/src/index.ts
+++ draw-app/apps/http-backend/src/index.ts
@@ -1,5 +1,56 @@
 import express from "express"
+import * as z from "zod"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config"
+import { middleware } from "./middleware"
 
 const app = express()
+app.use(express.json())
+
+const userValidation = z.object({
+    username: z.email(),
+    password: z.string()
+})
+
+type UserValidation = z.infer<typeof userValidation>
+
+app.post("/signup", (req, res) => {
+    const result = userValidation.safeParse(req.body);
+    if(!result.success) {
+        return res.status(400).json({
+            message: "incorrect format"
+        })
+    }
+    const {username, password} = result.data;
+
+    res.json({
+        userId: 123
+    })
+})
+
+app.post("signin", (req, res) => {
+    const result = userValidation.safeParse(req.body);
+    if(!result.success) {
+        return res.status(400).json({
+            message: "incorrect format"
+        })
+    }
+    const {username, password} = result.data;
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET)
+
+    res.json({
+        token
+    })
+})
+
+app.post("room", middleware,  (req, res) => {
+    res.json({
+        roomId: 123
+    })
+})
 
 app.listen(3002)
--- draw-app/apps/ws-backend/src/index.ts
+++ draw-app/apps/ws-backend/src/index.ts
@@ -1,10 +1,35 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
 
-const wss = new WebSocketServer({ port: 8080})
+const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', (ws) => {
-    ws.send("ka mantes")
-    ws.on('message', (data) => {
-        ws.send('pong')
-    })
-})
+interface CustomJwtPayload extends JwtPayload {
+  userId: string;
+}
+
+wss.on("connection", (ws, request) => {
+  const url = request.url;
+  if (!url) {
+    return;
+  }
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token");
+
+  if (!token) {
+    ws.close();
+    return;
+  }
+
+  const decoded = jwt.verify(token, JWT_SECRET) as CustomJwtPayload;
+
+  if (!decoded || !decoded.userId) {
+    ws.close();
+    return;
+  }
+
+  ws.send("ka mantes");
+  ws.on("message", (data) => {
+    ws.send("pong");
+  });
+});
