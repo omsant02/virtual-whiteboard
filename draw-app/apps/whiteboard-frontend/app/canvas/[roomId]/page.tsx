3dfad86b264abbb10a583b19c915c0ba43ea331d
om santoshwar <omsantoshwar02@gmail.com>
2026-02-01 21:40:18 +0800

    refactor, generalise canvas logic

--- draw-app/apps/whiteboard-frontend/app/canvas/[roomId]/page.tsx
+++ draw-app/apps/whiteboard-frontend/app/canvas/[roomId]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react";
 
 export default function Canvas() {
@@ -7,45 +8,8 @@ export default function Canvas() {
 
   useEffect(() => {
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
 
-      if (!ctx) {
-        return;
-      }
-
-      ctx.fillStyle = "rgba(0,0,0)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-      let clicked: boolean;
-      let startX: number;
-      let startY: number;
-
-      canvas.addEventListener("mousedown", (e) => {
-        clicked = true;
-        console.log(e.clientX);
-        console.log(e.clientY);
-        startX = e.clientX;
-        startY = e.clientY;
-      });
-
-      canvas.addEventListener("mouseup", (e) => {
-        clicked = false;
-        console.log(e.clientX);
-        console.log(e.clientY);
-      });
-
-      canvas.addEventListener("mousemove", (e) => {
-        if (clicked) {
-          const width = e.clientX - startX;
-          const height = e.clientY - startY;
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.fillStyle= "rgba(0,0,0)"
-          ctx.fillRect(0,0,canvas.width, canvas.height)
-          ctx.strokeStyle = "rgba(255,255,255)";
-          ctx.strokeRect(startX, startY, width, height);
-        }
-      });
+      initDraw(canvasRef.current)
     }
   }, [canvasRef]);
 
